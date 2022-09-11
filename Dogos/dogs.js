96988c6d5a13ae7a776745e654f9434cd23782a9
orzolka <adrianna.orzol@gmail.com>
2022-09-11 19:11:47 +0200

    final version of doggo viewer

--- Dogos/dogs.js
+++ Dogos/dogs.js
@@ -1,5 +1,6 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random";
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const randomBreedUrl = "https://dog.ceo/api/breed/replace/images/random";
 
 fetch(imgUrl)
 .then(function(response){
@@ -9,6 +10,7 @@ return response.json();
     const img = document.createElement('img');
     img.src = data.message;
     img.alt = "Doggo image";
+    img.classList.add = "randomBreedImg"
     document.querySelector(".container").appendChild(img)
 })
 
@@ -28,7 +30,6 @@ fetch(breedUrl).then(function(response){
 
 document.querySelector("#breed").addEventListener('change',function(event){
    let selectedBreed = event.target.value;
-   const randomBreedUrl = "https://dog.ceo/api/breed/replace/images/random";
    let selectedBreedUrl = randomBreedUrl.replace("replace",selectedBreed);
    document.querySelector(".emoji").style.display = "block";
    fetch(selectedBreedUrl)
@@ -40,13 +41,7 @@ document.querySelector("#breed").addEventListener('change',function(event){
         const img = document.createElement('img');
         img.src = data.message;
         img.alt = "Image of a dog of selected breed";
-        imgToReplace.replaceChild(img, imgToReplace.firstElementChild);
-        document.querySelector(".emoji").style.display = "none";
+        imgToReplace.replaceChild(img, imgToReplace.lastChild);
+       document.querySelector(".emoji").style.display = "none";
     })
 })
-
-
-const loadingEmoji = function(){
-    document.addEventListener("DOMContentLoaded", () => {
-        alert("Loaded!")
-    })}
