d5bfbaa5078902a75a3ee5c2313d0badbf863c58
orzolka <adrianna.orzol@gmail.com>
2022-09-18 18:50:04 +0200

    Update mole.js
    Fixed show&hide

--- Feed a mole game/mole.js
+++ Feed a mole game/mole.js
@@ -6,47 +6,34 @@ let molesArr = Object.values(molesAll);
 
 // Choose random mole, display for random n sec, hide after random n sec
 
-
+function returnRandomMole(){
+    let randomMole= Math.floor(Math.random()*(molesAll.length));
+    return randomMole;  
+}
 const show = function(moleToShow){
+    let randomTime =(Math.random()*5000) + 2000;
         molesArr[moleToShow].style.display = "block" 
+        setTimeout(() => {
+            hide(moleToShow);
+        }, randomTime);
         }
-        
-        
+                  
 const hide = function(moleToHide){
         molesArr[moleToHide].style.display = "none" 
         }
-
-
-
-
-/* const hide = function(moleToHide){
-    let showTime = (Math.floor((Math.random()*3)+2))*1000;
-    if(Date.now()>(Date.now() + showTime)){
-
-        moleToHide.style.display = "none"
+  let runAgainAt = Date.now();
+  function showHide() {
+    if (Date.now() > runAgainAt) {
+        let randomTime = Math.random()*5000
+        show(returnRandomMole());
+         runAgainAt = Date.now() + randomTime;
     }
-    } */
-
-setInterval(function(){
-    let randomMole= Math.floor(Math.random()*(molesAll.length));
-    let randTime = (Math.floor(Math.random()*4000)) + 2000;
-    show(randomMole);
-    setInterval(function(){
-        hide(randomMole)
-    },randTime)
-
-    }
-    ,(1000*Math.floor(Math.random()*4))
- 
-) 
+    requestAnimationFrame(showHide);
+  }
+  requestAnimationFrame(showHide);
 
 
-
-
-
-
-
-//hide(molesArr[2])
+  
 
 //Display them for n to max 4 (random) sec and then hide
 
